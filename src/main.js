1daf5bb7c16689f3ac525ff868edec6f90f1b21c
skyloyal <913531825@qq.com>
2021-09-28 11:51:09 +0800

    安装elementui和配置babel对它的支持

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {
+  Button
+} from 'element-ui'
 Vue.config.productionTip = false
-
+Vue.component(Button.name, Button)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
